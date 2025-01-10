b5a3dc546094c40498e764645a1b9c20c8b0ce46
Shr1mp <1205874457@qq.com>
2025-01-10 21:36:08 +0800

    first commit - created the project by fabric generator

---
+++ src/client/java/com/shr1mp4zh/fmod/Shr1mpfmodClient.java
@@ -0,0 +1,10 @@
+package com.shr1mp4zh.fmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Shr1mpfmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
